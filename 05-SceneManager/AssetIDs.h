d91a8edc02abde46c026f0117cdb678d4f3a332c
khanhsd0901 <khanhsd0901@gmail.com>
2022-05-08 22:40:31 +0700

    chinh lai scene

--- 05-SceneManager/AssetIDs.h
+++ 05-SceneManager/AssetIDs.h
@@ -227,10 +227,5 @@
 #define HUD_HEIGHT 32
 
 //windown
-#define WINDOW_CLASS_NAME L"GameWindow"
-#define MAIN_WINDOW_TITLE L"Super Mario Bros 3"
 
-#define BACKGROUND_COLOR D3DCOLOR_XRGB(0, 0, 0)
-#define SCREEN_WIDTH 272
-#define SCREEN_HEIGHT 256
 
